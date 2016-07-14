658a74b5e5cc49839dd13fe45e981b7ed1003ee6
Jianming Sang <jmsang@gatech.edu>
2016-07-13 23:26:58 -0400

    create registration form

---
+++ protocol/protocol/forms.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.forms import UserCreationForm
+from models import ProtocolUser
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+
+class RegistrationForm(UserCreationForm, forms.ModelForm):
+
+	class Meta:
+		model = ProtocolUser
+		fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+		fields_required = ['first_name', 'last_name']
+
+
+ 
+
+	def clean(self):
+		username = self.cleaned_data.get('username')
+		email = self.cleaned_data.get('email')
+
+		if ProtocolUser.objects.filter(username=username):
+			self.add_error('username', 'username has been used, please try another one')
+		if ProtocolUser.objects.filter(email=email):
+			self.add_error('email', 'this email address has been registered')
+
+		return self.cleaned_data
+
+	def save(self):
+		first_name = self.cleaned_data.get('first_name')
+		last_name = self.cleaned_data.get('last_name')
+		username = self.cleaned_data.get('username')
+		email = self.cleaned_data.get('email')
+		password = self.cleaned_data('password')
+
+
+
+		
